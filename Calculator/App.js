9972a7936db58a48cb50de10a66d34130546b81d
teemulin <63881696+teemulin@users.noreply.github.com>
2020-10-28 13:05:06 +0200

    calculator v0.5

---
+++ Calculator/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [sum, setSum] = useState('');
+
+  const sumButton = () => {
+    setSum('Result: ' + (parseFloat(num1) + parseFloat(num2)));
+  }
+
+  const minusButton = () => {
+    setSum('Result: ' + (parseFloat(num1) - parseFloat(num2)));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{sum}</Text>
+      <TextInput
+        style={styles.textinput}
+        keyboardType="numeric"
+        onChangeText={num1 => setNum1(num1)}
+        value={num1}
+      />
+      <TextInput 
+        style={styles.textinput}
+        keyboardType="numeric"
+        onChangeText={num2 => setNum2(num2)}
+        value={num2}
+      />
+      <Button onPress={sumButton} title="+" />
+      <Button onPress={minusButton} title="-" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textinput: {
+    width: 150,
+    borderColor: 'grey',
+    borderWidth: 1,
+  },
+});
