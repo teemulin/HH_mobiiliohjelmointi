90340e20c308c6435dec442c1c3cde3ff6fa126c
teemulin <63881696+teemulin@users.noreply.github.com>
2020-11-01 22:44:23 +0200

    shopping list v.1.0

--- Shopping_list/App.js
+++ Shopping_list/App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const addButton = () => {
+    setData([...data, {key: text}]);
+    setText('');
+  }
+
+  const clearButton = () => {
+    setData([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
+        <TextInput style={styles.textinput}
+          onChangeText={text => setText(text)}
+          value={text}
+        />
+        <View style={styles.buttons}>
+          <Button onPress={addButton} title='Add' />
+          <Button onPress={clearButton} title='Clear' />
+        </View>
+      </View>
+      <Text style={{fontSize: 18, color: 'blue'}}>Shopping list</Text>
+      <FlatList 
+        data={data}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
     </View>
   );
 }
@@ -17,5 +42,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  buttons: {
+    flexDirection: 'row',
+  },
+
+  textinput: {
+    width: 200,
+    borderColor: 'grey',
+    borderWidth: 1,
   },
 });
