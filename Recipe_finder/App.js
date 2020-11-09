15c49c645c74edbddee1ed7af81b7182dbd1c26a
teemulin <63881696+teemulin@users.noreply.github.com>
2020-11-09 19:14:14 +0200

    v.0.1 need some fixing still

--- Recipe_finder/App.js
+++ Recipe_finder/App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [desc, setDesc] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = () => {
+    const url = `http://www.recipepuppy.com/api/?i=${desc}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setRecipes(data);
+    })
+    .catch((error) => {
+      Alert.alert('Error', error);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.input}> 
+        <TextInput 
+          placeholder="Incredient"
+          onChangeText={desc => setDesc(desc)}
+          value={desc}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button title='Search' onPress={getRecipes} />
+      </View>
+      <View style={styles.results}>
+        <FlatList
+          keyExtractor={item => item.href} 
+          renderItem={({ item }) => <Text>{item.title}</Text>}
+          data={recipes}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +49,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    fontSize: 16,
+    width: 200,
+    padding: 10,
+  },
+  button: {
+
+  },
+  results: {
+    width: 200,
+    marginLeft: '5%',
   },
 });
