6b45fbeac13ebf355c99331de5ffb7e1e83ea5f5
teemulin <63881696+teemulin@users.noreply.github.com>
2020-12-15 17:07:27 +0200

    v.1.0

--- Text_to_speech/App.js
+++ Text_to_speech/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
+  const speak = () => {
+    if(!text=='') {
+      Speech.speak(text);
+    }
+    else {
+      Speech.speak('You forgot to type something')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input} 
+        placeholder='What do you want me to say?'
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Button onPress={speak} title='Press to hear text' />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +36,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 200,
+    textAlign: 'center',
   },
 });
