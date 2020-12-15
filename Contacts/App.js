a444a77ce4c54f318eba1b37a6b4c7cc3fa106fd
teemulin <63881696+teemulin@users.noreply.github.com>
2020-12-15 16:47:21 +0200

    v.1.0

--- Contacts/App.js
+++ Contacts/App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+
+  const [data, setData] = useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          
+        });
+
+        if (data.length > 0) {
+          setData(data);
+        }
+      }
+    })();
+  }, []);
+
+  const showContacts = () => {
+    setContacts(data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList 
+        data={contacts}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => {
+          return (
+            <View>
+              <Text>{item.name} {item.phoneNumbers[0].digits} </Text>
+            </View>
+          )
+        }}
+      />
+      <Button onPress={showContacts} title='Show contacts' />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +52,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 100,
   },
 });
